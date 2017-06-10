59ec6c9ef38c905497af978bdc2b38d313f91f9b
HaArLiNsH <haarlinsh@hotmail.com>
2017-06-10 13:53:30 +0200

    initialisation

---
+++ Sources/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("TextureReplacer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TextureReplacer")]
+[assembly: AssemblyCopyright("© 2013-2017 Davorin Učakar, Ryan Bray, RangeMachine")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("2.6.0.0")]
+
+[assembly: KSPAssembly("Texture Replacer", 2, 6)]
