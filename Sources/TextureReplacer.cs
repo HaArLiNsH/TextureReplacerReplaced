ccf82c551f5253250d29df8179e38bad0c6b5c4e
Christian Bronk <chris.github@bronk.de>
2017-06-11 21:39:55 +0200

    add basic Shader loading and accessing function.

--- Sources/TextureReplacer.cs
+++ Sources/TextureReplacer.cs
@@ -23,16 +23,18 @@
 
 using System.Reflection;
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace TextureReplacer
 {
-    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    [KSPAddon(KSPAddon.Startup.MainMenu, true)]
     public class TextureReplacer : MonoBehaviour
     {
         // Status.
         public static bool isInitialised = false;
 
         public static bool isLoaded = false;
+        private static Dictionary<string, Shader> allShaders = new Dictionary<string, Shader>();
 
         public void Start()
         {
@@ -42,6 +44,7 @@ namespace TextureReplacer
 
             isInitialised = false;
             isLoaded = false;
+            LoadShaders();
 
             if (Reflections.instance != null)
                 Reflections.instance.destroy();
@@ -85,5 +88,41 @@ namespace TextureReplacer
                 Destroy(this);
             }
         }
+
+        /// <summary>
+        /// Load all known shaders into the internal lookup table
+        /// </summary>
+        internal static void LoadShaders()
+        {
+            // force unity to load up any shader, even they are not used by any material on any asset
+            Shader.WarmupAllShaders();
+            foreach (var shader in Resources.FindObjectsOfTypeAll<Shader>())
+            {
+                if (!allShaders.ContainsKey(shader.name))
+                {
+                    allShaders.Add(shader.name, shader);
+                    //Util.log("loaded shader: " + shader.name);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Drop in replacement for Shader.find(). Shader.find() cannot see not buildin shaders
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        internal static Shader GetShader(string name)
+        {
+            if (allShaders.ContainsKey(name))
+            {
+                return allShaders[name];
+            }
+            else
+            {
+                Util.log("shader: " + name + " not found: ");
+                return null;
+            }
+        }
+
     }
 }
